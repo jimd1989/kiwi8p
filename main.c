8896bb9b4df4c57ddbeaeac8f3192faff11039a1
jimd1989 <jamesdalrymple@protonmail.com>
2022-10-17 18:44:22 +1000

    Command alphabets

--- main.c
+++ main.c
@@ -1,25 +1,50 @@
+#include <err.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "sysex.h"
 
-static void editParam(const SysexParam p, uint16_t v) {
+#define BUF_SIZE 4906
+
+static void runCmd(char, char, char *);
+static void editParam(uint8_t, uint16_t);
+
+/* input transformers */
+uint16_t readInt(char *s) {
+  return (uint16_t)atoi(s);
+}
+
+/* core functions */
+static void runCmd(char l, char m, char *arg) {
+  uint8_t n = ((uint8_t)l - 65) & ALPHABET_SIZE;
+  Cmd cmd = m == ' ' ? CMDS_PURE[n]  :
+            m == '.' ? CMDS_DOT[n]   :
+            m == ':' ? CMDS_COLON[n] :
+            m == '@' ? CMDS_AT[n]    : CMDS_PURE[ALPHABET_SIZE - 1];
+  uint16_t v = cmd.f(arg);
+  editParam(cmd.n, v);
+}
+
+static void editParam(uint8_t p, uint16_t v) {
+  if (p == UNKNOWN_FUNC) {
+    warnx("%s", "got unknown input");
+    return;
+  }
   sysexMsg[SYSEX_PARAM_IX] = p;
-  sysexMsg[SYSEX_VAL_UPPER_IX] = v >> 7;
+  sysexMsg[SYSEX_VAL_UPPER_IX] = (v >> 7) & 4095;
   sysexMsg[SYSEX_VAL_LOWER_IX] = v & 127;
   fwrite(sysexMsg, SYSEX_MSG_SIZE, 1, stdout);
   fflush(stdout);
-  return;
 }
 
 int main(void) {
-  char buf[4096] = {0};
+  char buf[BUF_SIZE] = {0};
   uint16_t n = 0;
   while (1) {
-    fgets(buf, 4096, stdin);
-    n = atoi(buf);
-    editParam(VCF_CUTOFF, n);
+    fgets(buf, BUF_SIZE, stdin);
+    /* split semicolons */
+    /* split letter:modifier:arg */
   }
   return 0;
 }
--- sysex.h
+++ sysex.h
@@ -13,11 +13,6 @@
 /* Command id here */
 #define SYSEX_EDIT_PARAM 14
 /* Data here */
-typedef enum SysexParam {
-  VCF_CUTOFF  = 40,
-  VCF_RES     = 42,
-  VCF_LFO_LVL = 44
-} SysexParam;
 #define VOICE_NUM 1
 #define SYSEX_FOOTER 247
 /* Full cmd */
@@ -25,6 +20,7 @@ typedef enum SysexParam {
 #define SYSEX_PARAM_IX 9
 #define SYSEX_VAL_UPPER_IX 10
 #define SYSEX_VAL_LOWER_IX 11
+
 static uint8_t sysexMsg[SYSEX_MSG_SIZE] = {
   SYSEX_START,
   MANUFACTURER_ID_1, MANUFACTURER_ID_2, MANUFACTURER_ID_3,
@@ -38,104 +34,368 @@ static uint8_t sysexMsg[SYSEX_MSG_SIZE] = {
   SYSEX_FOOTER
 };
 
+/* Command type */
+typedef uint16_t (*Transformer)(char *s);
+
+typedef struct Cmd {
+  uint8_t       n;
+  Transformer   f;
+} Cmd;
+
+/* Transformer declarations */
+uint16_t readInt(char *);
+
+#define UNKNOWN_FUNC 128
+
+/* Alphabets */
+#define ALPHABET_SIZE 59
+
+static const Cmd CMDS_PURE[ALPHABET_SIZE] = {
+  { 109, readInt}, /* A */
+  { UNKNOWN_FUNC, readInt}, /* B */
+  { 38, readInt}, /* C */
+  { 94, readInt}, /* D */
+  { 37, readInt}, /* E */
+  { UNKNOWN_FUNC, readInt}, /* F */
+  { UNKNOWN_FUNC, readInt}, /* G */
+  { UNKNOWN_FUNC, readInt}, /* H */
+  { UNKNOWN_FUNC, readInt}, /* I */
+  { UNKNOWN_FUNC, readInt}, /* J */
+  { UNKNOWN_FUNC, readInt}, /* K */
+  { UNKNOWN_FUNC, readInt}, /* L */
+  { UNKNOWN_FUNC, readInt}, /* M */
+  { 95, readInt}, /* N */
+  { UNKNOWN_FUNC, readInt}, /* O */
+  { UNKNOWN_FUNC, readInt}, /* P */
+  { 65, readInt}, /* Q */
+  { 93, readInt}, /* R */
+  { 113, readInt}, /* S */
+  { 114, readInt}, /* T */
+  { 66, readInt}, /* U */
+  { 34, readInt}, /* V */
+  { 92, readInt}, /* W */
+  { 67, readInt}, /* X */
+  { 36, readInt}, /* Y */
+  { 68, readInt}, /* Z */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { 77, readInt}, /* a */
+  { 118, readInt}, /* b */
+  { 25, readInt}, /* c */
+  { 78, readInt}, /* d */
+  { 23, readInt}, /* e */
+  { 40, readInt}, /* f */
+  { UNKNOWN_FUNC, readInt}, /* g */
+  { UNKNOWN_FUNC, readInt}, /* h */
+  { 107, readInt}, /* i */
+  { UNKNOWN_FUNC, readInt}, /* j */
+  { 81, readInt}, /* k */
+  { 22, readInt}, /* l */
+  { UNKNOWN_FUNC, readInt}, /* m */
+  { UNKNOWN_FUNC, readInt}, /* n */
+  { 104, readInt}, /* o */
+  { 105, readInt}, /* p */
+  { 53, readInt}, /* q */
+  { 80, readInt}, /* r */
+  { 79, readInt}, /* s */
+  { 21, readInt}, /* t */
+  { 54, readInt}, /* u */
+  { 49, readInt}, /* v */
+  { 20, readInt}, /* w */
+  { 55, readInt}, /* x */
+  { 24, readInt}, /* y */
+  { 56, readInt}, /* z */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+};
+
+static const Cmd CMDS_DOT[ALPHABET_SIZE] = {
+  { 110, readInt}, /* A. */
+  { UNKNOWN_FUNC, readInt}, /* B. */
+  { UNKNOWN_FUNC, readInt}, /* C. */
+  { 98, readInt}, /* D. */
+  { UNKNOWN_FUNC, readInt}, /* E. */
+  { UNKNOWN_FUNC, readInt}, /* F. */
+  { UNKNOWN_FUNC, readInt}, /* G. */
+  { UNKNOWN_FUNC, readInt}, /* H. */
+  { UNKNOWN_FUNC, readInt}, /* I. */
+  { UNKNOWN_FUNC, readInt}, /* J. */
+  { UNKNOWN_FUNC, readInt}, /* K. */
+  { UNKNOWN_FUNC, readInt}, /* L. */
+  { UNKNOWN_FUNC, readInt}, /* M. */
+  { 99, readInt}, /* N. */
+  { UNKNOWN_FUNC, readInt}, /* O. */
+  { UNKNOWN_FUNC, readInt}, /* P. */
+  { 69, readInt}, /* Q. */
+  { 97, readInt}, /* R. */
+  { 119, readInt}, /* S. */
+  { 116, readInt}, /* T. */
+  { 70, readInt}, /* U. */
+  { 35, readInt}, /* V. */
+  { 96, readInt}, /* W. */
+  { 71, readInt}, /* X. */
+  { UNKNOWN_FUNC, readInt}, /* Y. */
+  { 72, readInt}, /* Z. */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { 82, readInt}, /* a. */
+  { 111, readInt}, /* b. */
+  { 32, readInt}, /* c. */
+  { 83, readInt}, /* d. */
+  { 30, readInt}, /* e. */
+  { 42, readInt}, /* f. */
+  { UNKNOWN_FUNC, readInt}, /* g. */
+  { UNKNOWN_FUNC, readInt}, /* h. */
+  { 108, readInt}, /* i. */
+  { UNKNOWN_FUNC, readInt}, /* j. */
+  { 86, readInt}, /* k. */
+  { 29, readInt}, /* l. */
+  { UNKNOWN_FUNC, readInt}, /* m. */
+  { UNKNOWN_FUNC, readInt}, /* n. */
+  { UNKNOWN_FUNC, readInt}, /* o. */
+  { UNKNOWN_FUNC, readInt}, /* p. */
+  { 57, readInt}, /* q. */
+  { 85, readInt}, /* r. */
+  { 84, readInt}, /* s. */
+  { 27, readInt}, /* t. */
+  { 58, readInt}, /* u. */
+  { UNKNOWN_FUNC, readInt}, /* v. */
+  { 26, readInt}, /* w. */
+  { 59, readInt}, /* x. */
+  { 31, readInt}, /* y. */
+  { 60, readInt}, /* z. */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+};
+
+static const Cmd CMDS_COLON[ALPHABET_SIZE] = {
+  { 121, readInt}, /* A: */
+  { UNKNOWN_FUNC, readInt}, /* B: */
+  { UNKNOWN_FUNC, readInt}, /* C: */
+  { 102, readInt}, /* D: */
+  { UNKNOWN_FUNC, readInt}, /* E: */
+  { UNKNOWN_FUNC, readInt}, /* F: */
+  { UNKNOWN_FUNC, readInt}, /* G: */
+  { UNKNOWN_FUNC, readInt}, /* H: */
+  { UNKNOWN_FUNC, readInt}, /* I: */
+  { UNKNOWN_FUNC, readInt}, /* J: */
+  { UNKNOWN_FUNC, readInt}, /* K: */
+  { UNKNOWN_FUNC, readInt}, /* L: */
+  { UNKNOWN_FUNC, readInt}, /* M: */
+  { 103, readInt}, /* N: */
+  { UNKNOWN_FUNC, readInt}, /* O: */
+  { UNKNOWN_FUNC, readInt}, /* P: */
+  { 73, readInt}, /* Q: */
+  { 101, readInt}, /* R: */
+  { 120, readInt}, /* S: */
+  { 117, readInt}, /* T: */
+  { 74, readInt}, /* U: */
+  { UNKNOWN_FUNC, readInt}, /* V: */
+  { 100, readInt}, /* W: */
+  { 75, readInt}, /* X: */
+  { UNKNOWN_FUNC, readInt}, /* Y: */
+  { 76, readInt}, /* Z: */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { 87, readInt}, /* a: */
+  { 112, readInt}, /* b: */
+  { 51, readInt}, /* c: */
+  { 88, readInt}, /* d: */
+  { UNKNOWN_FUNC, readInt}, /* e: */
+  { 39, readInt}, /* f: */
+  { UNKNOWN_FUNC, readInt}, /* g: */
+  { UNKNOWN_FUNC, readInt}, /* h: */
+  { UNKNOWN_FUNC, readInt}, /* i: */
+  { UNKNOWN_FUNC, readInt}, /* j: */
+  { 91, readInt}, /* k: */
+  { 50, readInt}, /* l: */
+  { UNKNOWN_FUNC, readInt}, /* m: */
+  { UNKNOWN_FUNC, readInt}, /* n: */
+  { UNKNOWN_FUNC, readInt}, /* o: */
+  { UNKNOWN_FUNC, readInt}, /* p: */
+  { 61, readInt}, /* q: */
+  { 90, readInt}, /* r: */
+  { 89, readInt}, /* s: */
+  { 28, readInt}, /* t: */
+  { 62, readInt}, /* u: */
+  { UNKNOWN_FUNC, readInt}, /* v: */
+  { UNKNOWN_FUNC, readInt}, /* w: */
+  { 63, readInt}, /* x: */
+  { 52, readInt}, /* y: */
+  { 64, readInt}, /* z: */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+};
+
+static const Cmd CMDS_AT[ALPHABET_SIZE] = {
+  { UNKNOWN_FUNC, readInt}, /* A@ */
+  { UNKNOWN_FUNC, readInt}, /* B@ */
+  { UNKNOWN_FUNC, readInt}, /* C@ */
+  { UNKNOWN_FUNC, readInt}, /* D@ */
+  { UNKNOWN_FUNC, readInt}, /* E@ */
+  { UNKNOWN_FUNC, readInt}, /* F@ */
+  { UNKNOWN_FUNC, readInt}, /* G@ */
+  { UNKNOWN_FUNC, readInt}, /* H@ */
+  { UNKNOWN_FUNC, readInt}, /* I@ */
+  { UNKNOWN_FUNC, readInt}, /* J@ */
+  { UNKNOWN_FUNC, readInt}, /* K@ */
+  { UNKNOWN_FUNC, readInt}, /* L@ */
+  { UNKNOWN_FUNC, readInt}, /* M@ */
+  { UNKNOWN_FUNC, readInt}, /* N@ */
+  { UNKNOWN_FUNC, readInt}, /* O@ */
+  { UNKNOWN_FUNC, readInt}, /* P@ */
+  { UNKNOWN_FUNC, readInt}, /* Q@ */
+  { UNKNOWN_FUNC, readInt}, /* R@ */
+  { 106, readInt}, /* S@ */
+  { UNKNOWN_FUNC, readInt}, /* T@ */
+  { UNKNOWN_FUNC, readInt}, /* U@ */
+  { UNKNOWN_FUNC, readInt}, /* V@ */
+  { UNKNOWN_FUNC, readInt}, /* W@ */
+  { UNKNOWN_FUNC, readInt}, /* X@ */
+  { UNKNOWN_FUNC, readInt}, /* Y@ */
+  { UNKNOWN_FUNC, readInt}, /* Z@ */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+  { UNKNOWN_FUNC, readInt}, /* a@ */
+  { UNKNOWN_FUNC, readInt}, /* b@ */
+  { 48, readInt}, /* c@ */
+  { UNKNOWN_FUNC, readInt}, /* d@ */
+  { 45, readInt}, /* e@ */
+  { UNKNOWN_FUNC, readInt}, /* f@ */
+  { UNKNOWN_FUNC, readInt}, /* g@ */
+  { UNKNOWN_FUNC, readInt}, /* h@ */
+  { UNKNOWN_FUNC, readInt}, /* i@ */
+  { UNKNOWN_FUNC, readInt}, /* j@ */
+  { 46, readInt}, /* k@ */
+  { 44, readInt}, /* l@ */
+  { UNKNOWN_FUNC, readInt}, /* m@ */
+  { UNKNOWN_FUNC, readInt}, /* n@ */
+  { UNKNOWN_FUNC, readInt}, /* o@ */
+  { UNKNOWN_FUNC, readInt}, /* p@ */
+  { UNKNOWN_FUNC, readInt}, /* q@ */
+  { UNKNOWN_FUNC, readInt}, /* r@ */
+  { UNKNOWN_FUNC, readInt}, /* s@ */
+  { 33, readInt}, /* t@ */
+  { UNKNOWN_FUNC, readInt}, /* u@ */
+  { UNKNOWN_FUNC, readInt}, /* v@ */
+  { UNKNOWN_FUNC, readInt}, /* w@ */
+  { UNKNOWN_FUNC, readInt}, /* x@ */
+  { 47, readInt}, /* y@ */
+  { UNKNOWN_FUNC, readInt}, /* z@ */
+  { UNKNOWN_FUNC, readInt}, /* ignored */
+};
+
 /*
- * o1w - 20
- * o1t - 21
- * o1l - 22
- * o1e - 23
- * o1d - 24
- * o1c - 25
- * o2w - 26
- * o2t - 27
- * o2T - 28
- * o2l - 29
- * o2e - 30
- * o2d - 31
- * o2c - 32
- * dt  - 33
- * m1v - 34
- * m2v - 35
- * m2d - 36
- * m2e - 37
- * m2c - 38
- * fh  - 39
- * fL  - 40
- * fr  - 42
- * fl  - 44
- * fe  - 45
- * fk  - 46
- * fd  - 47
- * fc  - 48
- * v   - 49
- * vl  - 50
- * vc  - 51
- * vd  - 52
- * M1s - 53
- * M1c - 54
- * M1  - 55
- * M1d - 56
- * M2s - 57
- * M2c - 58
- * M2  - 59
- * M2d - 60
- * M3s - 61
- * M3  - 62
- * M3c - 63
- * M3d - 64
- * M4s - 65
- * M4  - 66
- * M4c - 67
- * M4d - 68
- * M5s - 69
- * M5  - 70
- * M5c - 71
- * M5d - 72
- * M6s - 73
- * M6  - 74
- * M6c - 75
- * M6d - 76
- * e1a - 77
- * e1d - 78
- * e1s - 79
- * e1r - 80
- * e1k - 81
- * e2a - 82
- * e2d - 83
- * e2s - 84
- * e2r - 85
- * e2k - 86
- * e3a - 87
- * e3d - 88
- * e3s - 89
- * e3r - 90
- * e3k - 91
- * l1w - 92
- * l1r - 93
- * l1d - 94
- * l1c - 95
- * l2w - 96
- * l2r - 97
- * l2d - 98
- * l2c - 99
- * l3w - 100
- * l3r - 101
- * l3d - 102
- * l3c - 103
- * c   - 104
- * p   - 105
- * s   - 106
- * V1  - 107
- * V2  - 108
- * a   - 109
- * A   - 110
- * w   - 111
- * wt  - 112
- * S   - 113
- * t   - 114
- * ad  - 116
- * sd  - 117
- * wr  - 118
- * sc  - 119
- * st  - 120
- * F   - 121
+ * o1w - 20 - w
+ * o1t - 21 - t
+ * o1l - 22 - l
+ * o1e - 23 - e
+ * o1d - 24 - y
+ * o1c - 25 - c
+ * o2w - 26 - w.
+ * o2t - 27 - t.
+ * o2T - 28 - t:
+ * o2l - 29 - l.
+ * o2e - 30 - e.
+ * o2d - 31 - y.
+ * o2c - 32 - c.
+ * dt  - 33 - t@
+ * m1v - 34 - V
+ * m2v - 35 - V.
+ * m2d - 36 - Y
+ * m2e - 37 - E
+ * m2c - 38 - C
+ * fh  - 39 - f:
+ * fL  - 40 - f
+ * fr  - 42 - f.
+ * fl  - 44 - l@
+ * fe  - 45 - e@
+ * fk  - 46 - k@
+ * fd  - 47 - y@
+ * fc  - 48 - c@
+ * v   - 49 - v
+ * vl  - 50 - l: 
+ * vc  - 51 - c:
+ * vd  - 52 - y:
+ * M1s - 53 - q
+ * M1c - 54 - u
+ * M1  - 55 - x
+ * M1d - 56 - z
+ * M2s - 57 - q.
+ * M2c - 58 - u.
+ * M2  - 59 - x.
+ * M2d - 60 - z.
+ * M3s - 61 - q:
+ * M3  - 62 - u:
+ * M3c - 63 - x:
+ * M3d - 64 - z:
+ * M4s - 65 - Q
+ * M4  - 66 - U
+ * M4c - 67 - X
+ * M4d - 68 - Z
+ * M5s - 69 - Q.
+ * M5  - 70 - U.
+ * M5c - 71 - X.
+ * M5d - 72 - Z.
+ * M6s - 73 - Q:
+ * M6  - 74 - U:
+ * M6c - 75 - X:
+ * M6d - 76 - Z:
+ * e1a - 77 - a
+ * e1d - 78 - d
+ * e1s - 79 - s
+ * e1r - 80 - r
+ * e1k - 81 - k
+ * e2a - 82 - a.
+ * e2d - 83 - d.
+ * e2s - 84 - s.
+ * e2r - 85 - r.
+ * e2k - 86 - k.
+ * e3a - 87 - a:
+ * e3d - 88 - d:
+ * e3s - 89 - s:
+ * e3r - 90 - r:
+ * e3k - 91 - k:
+ * l1w - 92 - W
+ * l1r - 93 - R
+ * l1d - 94 - D
+ * l1c - 95 - N
+ * l2w - 96 - W.
+ * l2r - 97 - R.
+ * l2d - 98 - D.
+ * l2c - 99 - N.
+ * l3w - 100 - W:
+ * l3r - 101 - R:
+ * l3d - 102 - D:
+ * l3c - 103 - N:
+ * c   - 104 - o
+ * p   - 105 - p
+ * s   - 106 - S@
+ * V1  - 107 - i
+ * V2  - 108 - i.
+ * a   - 109 - A
+ * A   - 110 - A.
+ * w   - 111 - b.
+ * wt  - 112 - b:
+ * S   - 113 - S
+ * t   - 114 - T
+ * ad  - 116 - T.
+ * sd  - 117 - T:
+ * wr  - 118 - b
+ * sc  - 119 - S.
+ * st  - 120 - S:
+ * F   - 121 - A:
  */
